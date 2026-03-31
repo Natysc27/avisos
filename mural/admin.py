2ec96a75aaf2e60f18003d79e4b40f518d0cf2ba
Naty <restaurda@gmail.com>
2026-03-30 21:39:13 -0300

    criado o models e suas migrações

--- mural/admin.py
+++ mural/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Aviso
 
-# Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+
+@admin.register(Aviso)
+class AvisoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'categoria', 'data_criacao')
+    list_filter = ('categoria', 'data_criacao')
+    search_fields = ('titulo', 'conteudo')
+    date_hierarchy = 'data_criacao'
